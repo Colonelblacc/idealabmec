0436c5bcb7d776f9ea93381c88f0f2f916bcfbcd
Colonelblacc <colonelblackshawn@gmail.com>
2025-02-18 21:05:30 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,8 @@ document.getElementById("booking-form").addEventListener("submit", function(e) {
     const time = document.getElementById("time").value;
     alert(`You have booked ${equipment} for ${time}.`);
 });
+
+AOS.init({
+    duration: 1000,  // Animation duration
+    offset: 200,     // Start the animation when the element is 200px from the viewport
+});
